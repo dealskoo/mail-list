2d2e1b976df3d71ea760941e1628019d9bd70725
jabber <2898117012@qq.com>
2022-02-17 21:47:50 +0800

    fix: event

--- src/Events/Event.php
+++ src/Events/Event.php
@@ -3,9 +3,13 @@
 namespace Dealskoo\MailList\Events;
 
 use Dealskoo\MailList\Models\Email;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
 
 class Event
 {
+    use Dispatchable, InteractsWithSockets, SerializesModels;
     public $email;
 
     public function __construct(Email $email)
--- src/Events/Unsubscribed.php
+++ src/Events/Unsubscribed.php
@@ -2,6 +2,11 @@
 
 namespace Dealskoo\MailList\Events;
 
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
 class Unsubscribed extends Event
 {
+    use Dispatchable, InteractsWithSockets, SerializesModels;
 }
